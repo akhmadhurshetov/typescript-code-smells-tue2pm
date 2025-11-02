10edc057c48fb8d843aaf07cf50d7572dcca158c
akhmadhurshetov <122173327+akhmadhurshetov@users.noreply.github.com>
2025-11-02 23:47:08 +0500

    Add constants for game valueAdd constants file for game constantss
    Defines constants for game configuration.

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * Central place to define magic values for the game.
+ * `EMPTY_CELL` and `BOARD_DIMENSION` can be tweaked without touching game logic.
+ */
+export const EMPTY_CELL = ' ';
+export const BOARD_DIMENSION = 3 as const;
